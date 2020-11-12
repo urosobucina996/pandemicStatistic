dd1481073692ab5ee98e201c3ad72e5afad31f73
Serverside <obucina.uros.1@gmail.com>
2020-11-12 09:59:35 +0100

    Get numbers

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from modules import request
 
 
-request.parseHtml('https://www.worldometers.info/coronavirus/')
+try:
+    request.parseHtml('https://www.worldometers.info/coronavirus/')
+
+except Exception as e:
+    print(e)
---
+++ modules/request.py
@@ -0,0 +1,35 @@
+import requests as req
+from bs4 import BeautifulSoup as soup
+
+import sys 
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+
+def parseHtml(url):
+
+
+    # open connection and grab pages content
+    #uClient = uReq.Request(url,HEADERS)
+    #url = ''
+    # regex for url in test progress ^(https|http):\/\/((www)\.([a-z]+)|([a-z]+))\.([a-z]*)(\/[a-z-?=]*)*$
+
+    #if re.findall("^(http(s)?):\/\/(www\.)?((?!www)[a-z]{3,}\.[a-z]{2,})(\/[a-z-?=]*)*$",url) == []:
+    #    print('You must enter valid url')
+    #    loggingScrape.logging.error('Regex for url address failed.')
+    #    exit(0)
+    
+    uClient = req.get(url,HEADERS)
+    pageHtml = uClient.content
+
+    # 301 is redirection, may couse trouble
+    if uClient.status_code != 200:
+        print(f"Web site in not responding, server error {uClient.status_code}.")
+        exit(0) 
+
+#html parsing
+# Connect to the URL
+    parsedPage = soup(pageHtml,"html.parser")
+    tag = parsedPage.body
+    print(tag.findAll('div',{'class':'maincounter-number'}))
+    exit(0)
+    return tag.findAll('div',{'class':'maincounter-number'})
