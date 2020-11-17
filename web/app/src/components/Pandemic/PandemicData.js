582ac037c0ec412a8097fe8ed76f39773fe3dc8d
Serverside <obucina.uros.1@gmail.com>
2020-11-17 19:28:18 +0100

    Cut code second try

--- web/app/src/components/Pandemic/PandemicData.js
+++ web/app/src/components/Pandemic/PandemicData.js
@@ -5,6 +5,7 @@ import 'antd/dist/antd.css';
 import { Table } from 'antd';
 import HeaderCard from '../HeaderCard/HeaderCard';
 import columns from "./tableStructure";
+import headers from "./requestHeader";
 
 export default class PandemicReport extends React.Component {
     state = {
@@ -12,20 +13,8 @@ export default class PandemicReport extends React.Component {
         countries : []
       }
 
-    
-    headers = {
-        'Accept': 'application/json',
-        'Connection': 'keep-alive',
-        'Access-Control-Allow-Origin': '*',
-        'Accept-Language': 'en-US,en;q=0.9,sr;q=0.8',
-        'Cache-Control': 'no-cache',
-        'Sec-Fetch-Dest': 'document',
-        'Pragma': 'no-cache',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
-    };
-
     componentDidMount() {
-      axios.get(`http://localhost:5000/flask`,{ headers:this.headers})
+      axios.get(`http://localhost:5000/flask`,{ headers:headers})
         .then(res => {
           console.log(res.data);
           this.setState({
@@ -43,12 +32,6 @@ export default class PandemicReport extends React.Component {
           this.setState({
             countries : state
           });
-
-          console.log(this.state.countries);
-          // Get key of groupCountries
-          // this.state.data.groupByCountry.map(country =>{
-          //   console.log(Object.keys(country)[0]);
-          // });
           
         })
         .catch( err => {
---
+++ web/app/src/components/Pandemic/requestHeader.js
@@ -0,0 +1,10 @@
+export default {
+    'Accept': 'application/json',
+    'Connection': 'keep-alive',
+    'Access-Control-Allow-Origin': '*',
+    'Accept-Language': 'en-US,en;q=0.9,sr;q=0.8',
+    'Cache-Control': 'no-cache',
+    'Sec-Fetch-Dest': 'document',
+    'Pragma': 'no-cache',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+};
