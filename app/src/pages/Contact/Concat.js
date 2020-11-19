b9944218b1c07d62e18ae272ae1883c420b3c331
Serverside <obucina.uros.1@gmail.com>
2020-11-19 10:54:51 +0100

    Contact form

--- app/src/pages/Contact/Concat.js
+++ app/src/pages/Contact/Concat.js
@@ -1,7 +1,43 @@
+import { Form, Input, InputNumber, Button } from 'antd';
+
 export default function Contact() {
+
+    const layout = {
+        labelCol: { span: 7 },
+        wrapperCol: { span: 12 },
+      };
+      
+    const validateMessages = {
+        required: '${label} is required!',
+        types: {
+        email: '${label} is not a valid email!',
+        number: '${label} is not a valid number!',
+        },
+        number: {
+        range: '${label} must be between ${min} and ${max}',
+        },
+    };
+
+
     return (
         <div>
-            <h2>Contact</h2>
+            <h1 style={{textAlignVertical: "center",textAlign: "center"}}>Contact</h1>
+            <Form {...layout} name="nest-messages" validateMessages={validateMessages}>
+                <Form.Item name={['user', 'name']} label="Name" rules={[{ required: true }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'email']} label="Email" rules={[{ type: 'email' }]}>
+                    <Input />
+                </Form.Item>
+                <Form.Item name={['user', 'introduction']} label="Introduction">
+                    <Input.TextArea />
+                </Form.Item>
+                <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                    <Button type="primary" htmlType="submit">
+                    Submit
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     );
 }
