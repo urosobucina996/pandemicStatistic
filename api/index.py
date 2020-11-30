d8d3e0b1444786fbf5bd4e28f506545c345121ac
Serverside <obucina.uros.1@gmail.com>
2020-11-30 10:41:42 +0100

    Graphene imported (testing)

--- api/index.py
+++ api/index.py
@@ -1,26 +1,30 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
-from graphene import ObjectType, String, Schema
+from graphene import ObjectType, Schema, Field, List
 from modules import scraper
 import time
 import json
-
+from models.worldwidedata import WorldWideData
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
-class Query(ObjectType):
-    # this defines a Field `hello` in our Schema with a single Argument `name`
-    hello = String(name=String(default_value="stranger"))
-    goodbye = String()
+data = scraper.parseHtml('https://www.worldometers.info/coronavirus/')
+jsonData = json.loads(data)
 
-    # our Resolver method takes the GraphQL context (root, info) as well as
-    # Argument (name) for the Field and returns data for the query Response
-    def resolve_hello(root, info, name):
-        return f'Hello {name}!'
+class Query(ObjectType):  
 
-    def resolve_goodbye(root, info):
-        return 'See ya!'
+    class Meta:
+        description = 'Query to get all data.'
+        model = WorldWideData
+    
+    worldwide = Field(WorldWideData)
+
+    def resolve_worldwide(parent, info):
+
+        return jsonData
+        
+    
 
 schema = Schema(query=Query)
 
@@ -36,18 +40,11 @@ def start():
 
     print(time.time()-start," seconds")
 
-#@app.route('/graphql', methods=['POST'])
-#def graph():
-    #req_data = json.loads(request.get_data())
-    #print(type(request.get_data()))
-    #print(req_data['query'])
-    #result = schema.execute(req_data['query'])
-    #print(result.data)
-    #print(dir(result))
-    #return json.dumps(result.data)
-    #result = schema.execute(req_data['query'])
-    #print(result.data['hello'])
-    #return req_data
+@app.route('/graphql', methods=['POST'])
+def graph():
+    req_data = json.loads(request.get_data())
+    result = schema.execute(req_data['query'])
+    return json.dumps(result.data)
 
 if __name__ == '__main__':
    app.run(debug = True)
