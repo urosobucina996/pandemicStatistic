ac419b54d6114fb2120e12103d903881dec7cfb4
Serverside <obucina.uros.1@gmail.com>
2020-12-03 11:05:27 +0100

    Python code corrections

--- api/modules/scraper.py
+++ api/modules/scraper.py
@@ -9,7 +9,7 @@ HEADERS = {'User-Agent': '''Mozilla/5.0 (Macintosh;
                             AppleWebKit/537.36 (KHTML, like Gecko) 
                             Chrome/35.0.1916.47 Safari/537.36'''}
 
-def parseHtml(url):
+def parsehtml(url):
     
     uClient = req.get(url,HEADERS)
     pageHtml = uClient.content
